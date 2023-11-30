7918a4be8316fdc81a991f45e67b709b19dc99d5
Matheus Pacheco Cruz <pachecx35@gmail.com>
2023-11-30 08:59:39 -0300

    ajuste nas condições

--- main.js
+++ main.js
@@ -8,27 +8,49 @@ const textMonth = document.querySelector('.monthText');
 const textYear = document.querySelector('.yearText');
 
 
+
+const spanAlert = document.querySelector('.alertSpan');
+
+
+
 function calc(){
     let data = new Date();
-    let day = Day.value;
-    let month = Month.value;
-    let year = Year.value;
+    let day = Number(Day.value) 
+    let month = Number(Month.value)
+    let year =  Number(Year.value);
 
-    let daySis = data.getDay();
-    let monthSis = data.getMonth();
-    let yearSis = data.getFullYear();
-    
-    let resultYear = yearSis - year;
-    let resultMonth = monthSis - month;
-    let resultDay = daySis - day;
+    daySis = data.getDate();
+    monthSis = data.getMonth() + 1;
+    yearSis = data.getFullYear();
+    negative = day - daySis;
 
-    textDay.textContent = resultDay;
-    textMonth.textContent = resultMonth;
-    textYear.textContent = resultYear;
+    let alertSpan = spanAlert;
 
+    if(!day || !month || !year){
+        alertSpan.classList.remove('hide')
+        return
+    }
+
+    if(year > yearSis || month > 12 || day > 30){
+        console.log("teste validação")
+        return
+    }
+
+    if(month > monthSis){
+        textYear.textContent = yearSis - year -1;
+        textMonth.textContent = monthSis;
+        textDay.textContent = Math.abs(negative);
+        return
+    }
+
+    textYear.textContent = yearSis - year;
+    textMonth.textContent = month - monthSis;
+    textDay.textContent = Math.abs(negative);
+   
 } 
 
 btn.addEventListener('click', function(){
    calc()
-    console.log('teste')
 });
+
+//Falta colocar borda vermelha nos input quando as datas forem invalidas
